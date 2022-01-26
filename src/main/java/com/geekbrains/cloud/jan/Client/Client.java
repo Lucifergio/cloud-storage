a675d307b728de9571686fefa7b0f32e8649fc20
aleksey <lexasalnikov@gmail.com>
2022-01-26 15:44:38 +0500

    LoadProject v.0.1

---
+++ src/main/java/com/geekbrains/cloud/jan/Client/Client.java
@@ -0,0 +1,112 @@
+package com.geekbrains.cloud.jan.Client;
+
+import com.geekbrains.cloud.jan.Server.Handler;
+import com.geekbrains.cloud.jan.Server.Sender;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+
+
+import java.io.*;
+import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ResourceBundle;
+
+public class Client implements Initializable {
+
+    private static final int SIZE = 256;
+
+    private Path clientDir;
+    public ListView <String> clientList;
+    public ListView <String> serverList;
+
+    private DataInputStream is;
+    private DataOutputStream os;
+    private byte[] buffer;
+
+    /**
+     * Поток чтения с сервера.
+     */
+    private void readLoop() {
+        try {
+            while (true) {
+                String command = is.readUTF();
+                System.out.println("Received: " + command);
+
+                if (command.equals("#list#")) {
+
+                    Platform.runLater(() -> serverList.getItems().clear());
+                    int fileCount = is.readInt();
+
+                    for (int i = 0; i < fileCount; i++) {
+                        String fileName = is.readUTF();
+                        Platform.runLater(() -> serverList.getItems().add(fileName));
+                    }
+
+                }else if (command.equals("#file#")) {
+                    Sender.getFile(is, clientDir, SIZE, buffer);
+                    Platform.runLater(this::updateClientList);
+                }
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Обновление файлового окна клиента.
+     */
+    private void updateClientList() {
+        try {
+            clientList.getItems().clear();
+            Files.list(clientDir)
+                    .map(p -> p.getFileName().toString())
+                    .forEach(f -> clientList.getItems().add(f));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        try {
+            buffer = new byte[SIZE];
+            clientDir = Paths.get(System.getProperty("user.home"));
+            updateClientList();
+            Socket socket = new Socket("localhost", 8189);
+            System.out.println("Network created...");
+
+            is = new DataInputStream(socket.getInputStream());
+            os = new DataOutputStream(socket.getOutputStream());
+
+            Thread readThread = new Thread(this::readLoop);
+            readThread.setDaemon(true);
+            readThread.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Скачивание файла с сервера.
+     */
+    public void download(ActionEvent actionEvent) throws IOException {
+        String fileName = serverList.getSelectionModel().getSelectedItem();
+        os.writeUTF("#get_file#");
+        os.writeUTF(fileName);
+        os.flush();
+
+    }
+
+    /**
+     * Отправка файла на сервер.
+     */
+    public void upload(ActionEvent actionEvent) throws IOException {
+        String fileName = clientList.getSelectionModel().getSelectedItem();
+        Sender.sendFile(fileName, os, clientDir);
+    }
+}
---
+++ src/main/java/com/geekbrains/cloud/jan/Server/Handler.java
@@ -0,0 +1,71 @@
+package com.geekbrains.cloud.jan.Server;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.geekbrains.cloud.jan.Server.Sender.getFile;
+import static com.geekbrains.cloud.jan.Server.Sender.sendFile;
+
+public class Handler implements Runnable{
+
+    private static final int SIZE = 256;
+
+    private Path clientDir;
+    private DataInputStream is;
+    private DataOutputStream os;
+
+    private final byte[] buffer;
+
+
+    public Handler (Socket socket) throws IOException {
+        is = new DataInputStream(socket.getInputStream());
+        os = new DataOutputStream(socket.getOutputStream());
+        clientDir = Paths.get("root");
+        buffer = new byte[SIZE];
+
+    }
+
+    public void sendServerFiles() throws IOException {
+       List<String> files = Files.list(clientDir)
+                .map(p -> p.getFileName().toString())
+                .collect(Collectors.toList());
+       os.writeUTF("#list#");
+       os.writeInt(files.size());
+        for (String file : files) {
+            os.writeUTF(file);
+        }
+        os.flush();
+    }
+
+    @Override
+    public void run() {
+
+        InputStream in = null;
+        OutputStream out = null;
+
+        try {
+
+            while (true) {
+                String command = is.readUTF();
+                System.out.println("Received: " + command);
+                if (command.equals("#file#")) {
+                    getFile(is, clientDir, SIZE, buffer);
+                    sendServerFiles();
+                }else if (command.equals("#get_file#")) {
+                    String fileName = is.readUTF();
+                    sendFile(fileName, os, clientDir);
+
+                }
+
+                }
+        }catch (Exception e)  {
+            e.printStackTrace();
+        }
+    }
+    
+}
